1cbec9398342eaf7a2880a56ef538b14eed7744d
Rishabh Ranjan <rishabh9050@gmail.com>
2024-03-23 14:37:36 +0530

    Arrays Boilerplate correction

--- boiler-plate-test.cpp
+++ boiler-plate-test.cpp
@@ -1,26 +1,31 @@
-// Use 'boiled_plate' to bring up the boiler plate.
+// Use 'array_boiled' to bring up the boiler plate.
 
 #include <iostream>
 using namespace std;
-void print_array(int* arr,int n){
-    cout<<endl;
-    for(int i=0;i<n;i++){
-        cout<<*arr<<" ";
+void print_array(int *arr, int n)
+{
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << *arr << " ";
         *arr++;
     }
-    cout<<endl;
+    cout << endl;
 }
-void  read_array(int* arr, int n){
-    cout<<"Enter "<<n<<" numbers:"<<endl;
-    for(int i=0;i<n;i++){
-        cin>>*arr;
+void read_array(int *arr, int n)
+{
+    cout << "Enter " << n << " numbers:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> *arr;
         arr++;
     }
 }
-int main(){
-    int n;
-    cout<<"Enter array size: ";
-    cin>>n;
+int main()
+{
+    int n = 0;
+    cout << "Enter array size: ";
+    cin >> n;
     int arr[n];
     read_array(&arr[0], n);
     print_array(&arr[0], n);
